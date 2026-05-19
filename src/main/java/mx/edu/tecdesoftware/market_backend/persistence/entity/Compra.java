95835907ec96ab3b97927d9f66e1310aad62697c
Dave2826 <david.morales@tecdesoftware.edu.mx>
2026-05-19 08:32:38 -0600

    feat(persistance): add Producto and Compra JPA entities

---
+++ src/main/java/mx/edu/tecdesoftware/market_backend/persistence/entity/Compra.java
@@ -0,0 +1,31 @@
+package mx.edu.tecdesoftware.market_backend.persistence.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+
+public class Compra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private Integer idCompra;
+
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    @Column(name = "fecha")
+    private LocalDateTime fecha;
+
+    @Column(name = "medio_pago")
+    private String medioPago;
+
+    @Column(name = "comentario")
+    private String comentario;
+
+    @Column(name = "estado")
+    private String estado;
+
+}
